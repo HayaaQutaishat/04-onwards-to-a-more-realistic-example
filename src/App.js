4e11c181b4af45d4ab46dd14c2699326960c2518
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-17 11:32:50 +0100

    Alternative to minimize the number of dependencies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import useHttp from "./hooks/use-http";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const applyData = useCallback((task) => {
-    const loadedTasks = [];
-    for (const taskKey in task) {
-      loadedTasks.push({ id: taskKey, text: task[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  }, []);
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-    },
-    applyData
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
+    const applyData = (task) => {
+      const loadedTasks = [];
+      for (const taskKey in task) {
+        loadedTasks.push({ id: taskKey, text: task[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-489ae-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+      },
+      applyData
+    );
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
