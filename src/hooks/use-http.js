4b78d9b3a9e6245c50621025dec8e395d9d880c5
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-17 11:13:02 +0100

    Adding some parameters to configure the hook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -8,11 +8,16 @@ const useHttp = (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch();
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        headers: requestConfig.headers ? requestConfig.headers : {},
+      });
       if (!response.ok) {
         throw new Error("Request failed!");
       }
       const data = await response.json();
+      applyData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
